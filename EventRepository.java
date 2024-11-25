4be053bb1ed195479634c8f7b7744a482d594f10
Ravali-Tallapelly <166210373+Ravali-Tallapelly@users.noreply.github.com>
2024-11-25 09:50:43 +0530

    Delete EventRepository.java

--- EventRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.eventbooking;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.mycompany.eventbooking.entities.Event;
-public interface EventRepository extends JpaRepository<Event, Long> {
-    // Custom query methods (if needed) can be added here
-}
-
